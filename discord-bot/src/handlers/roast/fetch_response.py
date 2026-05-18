e996e90bf5327f7f6720bdc4a8219ecffb16c35d
TaherMustansir1929 <eren.jeager1929101@gmail.com>
2026-05-18 22:40:52 +0500

    fix zeos_sarcastic_cat_ai_url env var name

--- discord-bot/src/handlers/roast/fetch_response.py
+++ discord-bot/src/handlers/roast/fetch_response.py
@@ -2,7 +2,7 @@ import os
 
 import httpx
 
-BASE_URL = os.getenv("ZEOS_SARCASTIC_CAT_URL", "http://localhost:8001")
+BASE_URL = os.getenv("ZEOS_SARCASTIC_CAT_AI_URL", "http://localhost:8001")
 
 
 async def fetch_roast_api(message: str):
